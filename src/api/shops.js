e3314f62209d15f3c071555321cf15aa98ce4c9b
AnYuan <Journeyerrr@gmail.com>
2020-08-28 14:49:47 +0800

    修正接口

--- src/api/shops.js
+++ src/api/shops.js
@@ -1,9 +1,16 @@
 import request from '@/utils/request'
 import { pageSize } from '@/utils/content'
 
-export function shops(data) {
+export function shopsPage(data) {
   return request({
     url: '/admin/shop/index?page=' + data.page + '&pageSize=' + pageSize,
+    method: 'get'
+  })
+}
+
+export function shops() {
+  return request({
+    url: '/admin/shop/list',
     method: 'get'
   })
 }
